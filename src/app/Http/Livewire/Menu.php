ac369410fde1bb6ef159a4d1ecbe0d0d6778db26
tomoe <tm.717-ktsshm@au.com>
2025-02-15 07:51:20 +0900

    コミット

--- src/app/Http/Livewire/Menu.php
+++ src/app/Http/Livewire/Menu.php
@@ -4,22 +4,22 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 
-class Menu extends Component
-{
-    public $showModal = false;
+// class Menu extends Component
+// {
+//     public $showModal = false;
 
-    public function render()
-    {
-        return view('livewire.menu');
-    }
+//     public function render()
+//     {
+//         return view('livewire.menu');
+//     }
 
-    public function openModal()
-    {
-        $this->showModal = true;
-    }
+//     public function openModal()
+//     {
+//         $this->showModal = true;
+//     }
 
-    public function closeModal()
-    {
-        $this->showModal = false;
-    }
-}
+//     public function closeModal()
+//     {
+//         $this->showModal = false;
+//     }
+// }
